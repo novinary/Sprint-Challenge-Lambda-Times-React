d83ce8d56be94e33da484d0e5c73045cb94a6cdc
novinary <novina.dev@gmail.com>
2019-03-01 13:06:24 +0000

    filterCards  wip- so far the site is only returning cards on state

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types'
 
-const Cards = props => {
+const Cards = (props) => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
